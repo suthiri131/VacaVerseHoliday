623b537888bff11a9035d4bcc201e750453f05a5
Tasmiyah <tasmiyahtaj26@gmail.com>
2023-12-13 20:22:54 +0800

    l

--- frontend/_component/Header/header.tsx
+++ frontend/_component/Header/header.tsx
@@ -1,9 +1,21 @@
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Input,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  Avatar,
+} from "@nextui-org/react";
 import appLogo from "../../_assets/images/logo.png";
 import search from "../../_assets/images/search.png";
-import Image from "next/image"; 
-import '../Header/header.css'
+import Image from "next/image";
+import "../Header/header.css";
 export default function App() {
   const Logo = () => (
     <Image src={appLogo} alt="Logo" width={64} height={32} /> // Set width and height based on your design
@@ -15,13 +27,12 @@ export default function App() {
     <Navbar isBordered>
       <NavbarContent justify="start">
         <NavbarBrand className="mr-4">
-       <Logo/>
-          <p className="hidden sm:block font-bold text-inherit">ACME</p>
+          <Logo />
         </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-3">
+        <NavbarContent className=" sm:flex gap-3">
           <NavbarItem>
-            <Link color="foreground" href="#">
-              Features
+            <Link color="foreground" href="/">
+              Home
             </Link>
           </NavbarItem>
           <NavbarItem isActive>
@@ -30,8 +41,8 @@ export default function App() {
             </Link>
           </NavbarItem>
           <NavbarItem>
-            <Link color="foreground" href="#">
-              Integrations
+            <Link color="foreground" href="/post">
+              post
             </Link>
           </NavbarItem>
         </NavbarContent>
@@ -43,11 +54,12 @@ export default function App() {
             base: "max-w-full sm:max-w-[10rem] h-10",
             mainWrapper: "h-full",
             input: "text-small",
-            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+            inputWrapper:
+              "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
           }}
           placeholder="Type to search..."
           size="sm"
-          startContent={<SearchIcon/>}
+          startContent={<SearchIcon />}
           type="search"
         />
         <Dropdown placement="bottom-end">
