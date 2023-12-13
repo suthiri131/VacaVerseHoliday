9df991e8c07a538426930160489e2df9ba8b6553
Tasmiyah <tasmiyahtaj26@gmail.com>
2023-12-13 20:17:10 +0800

    my head hurts

--- frontend/_component/Header/header.tsx
+++ frontend/_component/Header/header.tsx
@@ -1,45 +1,84 @@
-'use client'
-import { Button } from '@nextui-org/react';
 import React from "react";
-import Link from "next/link";
-import Image from 'next/image';
-import LogoImage from "../../_assets/images/logo.png";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
+import appLogo from "../../_assets/images/logo.png";
+import search from "../../_assets/images/search.png";
+import Image from "next/image"; 
 import '../Header/header.css'
-const Logo = () => (
-  <Image
-    src={LogoImage}
-    alt="Logo"
-    width={70} // Set the width of the logo image
-    height={70} // Set the height of the logo image
-  />
-);
-
-export default function Header() {
+export default function App() {
+  const Logo = () => (
+    <Image src={appLogo} alt="Logo" width={64} height={32} /> // Set width and height based on your design
+  );
+  const SearchIcon = () => (
+    <Image src={search} alt="Logo" width={20} height={20} /> // Set width and height based on your design
+  );
   return (
-    <div className="w-full h-20 bg-emerald-800 sticky top-0">
-      <div className="container mx-auto px-4 h-full">
-        <div className="flex justify-between items-center h-full">
-          <Logo />
-          <ul className="hidden md:flex gap-x-6 text-white">
-            <li>
-              <Link href="/login">
-                <p>About Us</p>
-              </Link>
-            </li>
-            <li>
-              <Link href="/services">
-                <p>Services</p>
-              </Link>
-            </li>
-            <li>
-              <Link href="/contacts">
-                <p>Contacts</p>
-              </Link>
-            </li>
-          </ul>
-          <Button />
-        </div>
-      </div>
-    </div>
+    <Navbar isBordered>
+      <NavbarContent justify="start">
+        <NavbarBrand className="mr-4">
+       <Logo/>
+          <p className="hidden sm:block font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-3">
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Features
+            </Link>
+          </NavbarItem>
+          <NavbarItem isActive>
+            <Link href="#" aria-current="page" color="secondary">
+              Customers
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Integrations
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+      </NavbarContent>
+
+      <NavbarContent as="div" className="items-center" justify="end">
+        <Input
+          classNames={{
+            base: "max-w-full sm:max-w-[10rem] h-10",
+            mainWrapper: "h-full",
+            input: "text-small",
+            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+          }}
+          placeholder="Type to search..."
+          size="sm"
+          startContent={<SearchIcon/>}
+          type="search"
+        />
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">zoey@example.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">My Settings</DropdownItem>
+            <DropdownItem key="team_settings">Team Settings</DropdownItem>
+            <DropdownItem key="analytics">Analytics</DropdownItem>
+            <DropdownItem key="system">System</DropdownItem>
+            <DropdownItem key="configurations">Configurations</DropdownItem>
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
+    </Navbar>
   );
 }
